909bb4f964ee540be1d21827fdd2075a7099d0b4
sanketcodekul <sanketsb.18@gmail.com>
2022-03-05 07:58:59 +0530

    git basics

---
+++ index.js
@@ -0,0 +1,55 @@
+// var, let and const keywords to create a variable.
+
+// var keyword variable is a global scope and function scope variable 
+// let keyword variable is a local/block scope variable 
+
+//var content; var num1; let num2;
+
+content = "Welcome to JS";
+console.log(content);
+
+//num1=100;
+//console.log(num1);
+
+//console.log(num2);
+const num1 = 500;
+let num2;
+num2 = 1000;
+
+
+
+if (true) {
+    const PI = 3.14;
+    var content;
+    console.log(PI);
+
+
+
+
+    console.log(content)
+
+    // content = 222;
+
+    // console.log(content);
+}
+
+
+if (true) {
+   
+   // console.log('account number', account_no);
+}
+
+
+//console.log(PI);
+
+
+function displayMessage (){
+    var account_no = 123456;
+
+    account_no = 789654;
+   
+}
+
+console.log('account number', account_no);
+
+displayMessage()
